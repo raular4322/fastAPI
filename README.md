c73e0dde94fee4e45dc5069ba29fa66f1cb79789
raular4322 <raular98@hotmail.com>
2020-11-16 22:15:16 +0100

    ya esta

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fastAPI
+
+Este proyecto va a ser un URL Shortener que cumple con los requisitos del primer hito de la rama web de Core Academy
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Hello Web Development Team"}
+
+@app.get("/short/{url}")
+async def root(url: str):
+    web_url = "http://tinyurl.com/api-create.php?url=" + url
+    r = requests.get(web_url)
+    return r.text
